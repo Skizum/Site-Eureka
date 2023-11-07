0fe7cc3af0c1c03405fe4fbf34f3a6a3f3ac07b7
Skizum <118355944+Skizum@users.noreply.github.com>
2023-11-07 07:01:27 +0100

    image add
    add and import image

---
+++ SiteEureka/src/Constantes/Images.js
@@ -0,0 +1,44 @@
+ 
+import hero from "../assets/1logo.png";
+import back4 from "../assets/back4.jpg";
+import back5 from "../assets/back5.jpg";
+import background from "../assets/backgroud.png";
+import background1 from "../assets/background1.png";
+import background3 from "../assets/backgroung3.png";
+import banner3 from "../assets/banner3.png";
+import bou from "../assets/bou.png";
+import heroservice from "../assets/heroservice.jpg";
+import image1 from "../assets/image1.jpg";
+import image2 from "../assets/image2.jpg";
+import image3 from "../assets/image3.jpg";
+import marketing1 from "../assets/marketing1.png";
+import service1 from "../assets/service1.jpg";
+import service2 from "../assets/service2.jpg";
+import service3 from "../assets/service3.jpg";
+import test1 from "../assets/test 1.png";
+import visionmarketing from "../assets/visionmarketing.jpg";
+import taf1 from "../assets/taf1.jpg"
+import taf2 from "../assets/taf2.jpg"
+
+export const Images = {
+  taf1,
+  taf2,
+  hero,
+  back4,
+  back5,
+  background,
+  background1,
+  background3,
+  banner3,
+  bou,
+  heroservice,
+  image1,
+  image2,
+  image3,
+  marketing1,
+  service1,
+  service2,
+  service3,
+  test1,
+  visionmarketing,
+ }
---
+++ SiteEureka/src/Constantes/index.js
@@ -0,0 +1,4 @@
+export { Images } from './Images';
+
+
+
